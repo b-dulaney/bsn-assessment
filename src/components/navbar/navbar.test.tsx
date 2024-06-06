af138504c1910ac5303ebf91b76c6e03742d4064
b-dulaney <brendanfdulaney@gmail.com>
2024-06-06 14:09:52 -0600

    test: navbar active link tests

---
+++ src/components/navbar/navbar.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './navbar'
+
+describe('Navbar', () => {
+  it('renders the correct active link', () => {
+    render(
+      <MemoryRouter initialEntries={['/tags']}>
+        <Navbar />
+        <Routes>
+          <Route path="/tags">Tags Page</Route>
+        </Routes>
+      </MemoryRouter>
+    )
+    const activeLinkElement = screen.getByText('Tags')
+    expect(activeLinkElement).toHaveClass('text-pink-400')
+  })
+
+  it('renders the correct non-active links', () => {
+    render(
+      <MemoryRouter initialEntries={['/tags']}>
+        <Navbar />
+        <Routes>
+          <Route path="/tags">Tags Page</Route>
+        </Routes>
+      </MemoryRouter>
+    )
+    const nonActiveLinkElement = screen.getByText('Books')
+    expect(nonActiveLinkElement).toHaveClass('text-white')
+  })
+})
