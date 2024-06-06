d7d4ea708603dc6dbfabaa636aa7a209337b71e2
b-dulaney <brendanfdulaney@gmail.com>
2024-06-06 10:52:04 -0600

    fix: proper crossOrigin property for font import link

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
@@ -15,4 +19,4 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </head>
     <App />
   </React.StrictMode>
-);
+)
