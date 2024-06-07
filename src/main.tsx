adf94235716a4d81a1f9ff0929dbe463ca498405
b-dulaney <brendanfdulaney@gmail.com>
2024-06-07 08:41:51 -0600

    chore: update routing for add/edit books

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,21 @@ import App from './App.tsx'
 import './index.css'
 import Books from './pages/books'
 import Categories from './pages/categories/categories.tsx'
+import EditBook from './pages/edit-book/index.tsx'
 import Tags from './pages/tags/tags.tsx'
+import AddBook from './pages/add-book/index.tsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      { path: '/', element: <Books /> },
+      {
+        index: true,
+        element: <Books />,
+      },
+      { path: 'books/:id', element: <EditBook /> },
+      { path: 'books/add', element: <AddBook /> },
       { path: 'tags', element: <Tags /> },
       { path: 'categories', element: <Categories /> },
     ],
